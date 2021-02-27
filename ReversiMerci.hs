b4b809f72f5beb27f03378bfe4bebe8193e68d2f
habimana <rugwiroclement@gmail.com>
2021-02-26 20:44:14 -0800

    updated advanced Ai, ready for Reversi Merci

--- ReversiMerci.hs
+++ ReversiMerci.hs
@@ -1,10 +1,6 @@
 module ReversiMerci where
 
--- To run it, try:
--- ghci
--- :load MagicSum
-
-data State = State Board (Int,Int, Char, Char)  -- internal_state available_actions
+data State = State Board (Int,Int, Char, Char) 
          deriving (Ord, Eq, Show)
 
 data Result = EndOfGame Char State    -- end of game: value, starting state
@@ -15,21 +11,20 @@ type Game = Action -> State -> Result
 
 type Player = Char
 
-
------- The Magic Sum Game -------
-
 data Action = Action (Int, Int)                  -- a move for a player is just an Int
          deriving (Ord,Eq, Show)                 -- a move for a player is just an Int
 type Board = [[Char]]  -- (self,other)
 -- type InternalState = (Int,Int, Char, Char)
 
-
+--return the winner of the game given the characters and scores from the state
 check_winner s1 s2 p1 p2 
     |s1 == s2 = 't'
     |s1 > s2 = p1
     |otherwise = p2
 
-
+-- main game
+-- takes a move and a state and updates the state with the action.
+-- checks for the winning move
 reversi :: Game 
 reversi (Action (row,col)) (State board (mine, opp, player, other))
  | win board mine opp player other = EndOfGame (check_winner mine opp player other) reversi_start
@@ -43,18 +38,21 @@ reversi (Action (row,col)) (State board (mine, opp, player, other))
         else ContinueGame (State new_board (int1,int2, char1, char2))
  | otherwise = ContinueGame (State board (mine, opp, player, other))
 
- -- reversi (a (2,3)) test_end_State  
- --reversi (a (2,3)) reversi_start
-
-
--- updates the board
+-- updates the board with a play
+-- takes a (row, col) and the state, and updates the board with the current player's move
 updateBoard :: (Int, Int) -> State -> State
 updateBoard (row,col) (State board (mine, opp, player, other)) = 
     let preUpdateBoard = replaceNth1 row col player board 
         updatedBoard = updateBoardWithPlay preUpdateBoard row col player other
         (newMine, newOpp) = count updatedBoard
-    in (State updatedBoard (newOpp, newMine, other, player))
+    in 
+        if (player == 'X') 
+        then
+            (State updatedBoard (newOpp, newMine, other, player))
+        else 
+            (State updatedBoard (newMine, newOpp, other, player))
 
+-- count number of X and O values currently on the board
 count :: Board -> (Int,Int)
 count board = 
     let 
@@ -62,6 +60,8 @@ count board =
         y_count = foldr (\x y -> (length(filter (\y -> y=='O') x)) + y) 0 board
     in (x_count, y_count)
 
+-- check the board in all directions from (row, col) and update the board in all directions
+-- takes a board, row, col and updates the board by flipping the opponent (other) with player chips
 updateBoardWithPlay :: Board -> Int -> Int -> Player -> Player -> Board
 updateBoardWithPlay board row col player other = 
     let board1 = updateFollowUp board row col player other (\ i -> i) (\ j -> j+1)
@@ -75,26 +75,27 @@ updateBoardWithPlay board row col player other =
     in (board8)
 
 
-
-
+-- Helper for updateBoardwithPlay
 updateFollowUp :: Board -> Int -> Int -> Player -> Player -> (Int -> Int) -> (Int -> Int) -> Board
 updateFollowUp board row col player other fRow fCol
  | (checkBoard player other board row col fRow fCol) = flipMerci board player (fRow row) (fCol col) fRow fCol -- horizontal right
  | otherwise = board
 
+-- flip opponents chips after valid play in a given direction
 flipMerci :: Board -> Player -> Int -> Int -> (Int -> Int) -> (Int -> Int) -> Board
 flipMerci board player row col fRow fCol
- -- | (isOutOfBound row col) || (board !! row !! col) == '.' || (board !! row !! col) == player = board
  | (isOutOfBound row col) || (board !! row !! col) == '*' || (board !! row !! col) == player = board
  | otherwise = flipMerci (replaceNth1 row col player board) player (fRow row) (fCol col) fRow fCol
 
+-- replace a character in a 2D array
+-- calls replaceNth to update specific rows/cols
 replaceNth1 :: Int -> Int -> a -> [[a]] -> [[a]]
 replaceNth1 _ _ _ [] = []
 replaceNth1 row col newVal (x:xs)
  | row == 0 = replaceNth col newVal x : xs
  | otherwise = x:replaceNth1 (row-1) col newVal xs
 
--- replaces characters in board
+-- replaces the character at position n with newVal
 replaceNth _ _ [] = []
 replaceNth n newVal (x:xs)
    | n == 0 = newVal:xs
@@ -107,9 +108,6 @@ win board mine opp player other
  | noValidMoves board (createCoordinate [0..7] [0..7]) player other && noValidMoves board (createCoordinate [0..7] [0..7]) other player = True
  | otherwise = False
    where (newMine, newOpp) = count board
- -- win test_end_star 5 4 'X' 'O' 
-
-
 
 -- checks whether a player has any valid moves available on the board
 noValidMoves :: Board -> [(Int, Int)] -> Player -> Player -> Bool
@@ -119,14 +117,15 @@ noValidMoves board (h:t) player other
  | otherwise = noValidMoves board t player other
 
 -- checks if a move is valid
+    -- checks if a player's move will flip at least one of the opp's player schip
+        -- checks if there is a valid play in all directions from given (row, col) position
 valid :: (Int, Int) -> Board -> Player -> Player -> Bool
 valid (row, col) board player other = 
  ((checkPlay player other board row col (\ i -> i) (\ j -> j+1) (\ i -> i) (\j -> j-1)) || -- check horizontal
  (checkPlay player other board row col (\ i -> i+1) (\ j -> j) (\ i -> i-1) (\j -> j)) || -- check Vertical 
  (checkPlay player other board row col (\ i -> i-1) (\ j -> j-1) (\ i -> i+1) (\j -> j+1)) || -- check diagnal up
  (checkPlay player other board row col (\ i -> i+1) (\ j -> j-1) (\ i -> i-1) (\j -> j+1))) &&  -- check diagnal down
- -- (board !! row !! col) == '.'
- (board !! row !! col) == '*' 
+ (board !! row !! col) == '*'
 
 -- helper for valid
 checkPlay :: Char -> Char -> Board -> Int -> Int -> (Int -> Int) -> (Int -> Int) -> (Int -> Int) -> (Int -> Int) -> Bool
@@ -139,20 +138,19 @@ checkBoard player other board row col fRow fCol
  | ((board !! (fRow row) !! (fCol col)) == other) && not (isOutOfBound (fRow row) (fCol col)) = plaYable player board (fRow row) (fCol col) fRow fCol
  | otherwise = False
 
-
+-- Playable checks if in a given direction a play is valid
+    -- Returns true or false is a play is possible
 plaYable :: Char -> Board -> Int -> Int -> (Int -> Int) -> (Int -> Int) -> Bool
 plaYable player board row col fRow fCol
  | isOutOfBound row col = False
- -- | board !! row !! col == '.' = False
  | board !! row !! col == '*' = False
  | board !! row !! col == player = True
  | otherwise = plaYable player board (fRow row) (fCol col) fRow fCol
 
-
+--check if row and column are within bounds of board
 isOutOfBound :: Int -> Int -> Bool
 isOutOfBound row col = row > 7 || col > 7 || row < 0 || col < 0
  
-
 -- {check if board is full
 boardFull :: Board -> Bool
 boardFull [] = True
@@ -160,34 +158,29 @@ boardFull (h:t)
  | anyOpen h = False
  | otherwise = boardFull t
 
+--check if any open space in row
+    -- helper for checking if board is full
 anyOpen ::[Char] -> Bool
 anyOpen row = or [x == '*'| x <- row]
--- anyOpen row = or [x == '.'| x <- row]
 
+--takes a list of all coordinates and returns a vaid move
 returnValidMove :: [(Int, Int)] -> Board -> Player -> Player -> (Int, Int)
 returnValidMove [] _ _ _ = (8,8)
 returnValidMove (h:t) board player other
  | valid h board player other = h
  | otherwise = returnValidMove t board player other
 
-printState (ContinueGame (State board (mine, opp, player, other))) = show_board board
-printState1 (State board (mine, opp, player, other)) = show_board board
-
-
--- Simple Player ---
-simple_player :: State -> Result
-simple_player (State board (mine, opp, player, other))
- | noValidMoves board (createCoordinate [0..7] [0..7]) player other = ContinueGame (State board (opp, mine, other, player))
- | otherwise = reversi (Action (row, col)) (State board (mine, opp, player, other))
-    where (row, col) = returnValidMove (createCoordinate [0..7] [0..7]) board player other
+-- -- Simple Player ---
+-- simple_player :: State -> Result
+-- simple_player (State board (mine, opp, player, other))
+--  | noValidMoves board (createCoordinate [0..7] [0..7]) player other = ContinueGame (State board (opp, mine, other, player))
+--  | otherwise = reversi (Action (row, col)) (State board (mine, opp, player, other))
+--     where (row, col) = returnValidMove (createCoordinate [0..7] [0..7]) board player other
  
-
--- check if board is full
-createCoordinate lst lst1 =  [(x,y)| x <- lst, y <- lst1]
 -- createAllMoves = 
-show_board board= putStr (unlines [unwords [show (board !! y !! x) | x <- [0..7]] | y <- [0..7]])
+createCoordinate lst lst1 =  [(x,y)| x <- lst, y <- lst1]
 
-reversi_start = State [['*', '*', '*', '*', '*', '*', '*', '*'], 
+reversi_start = State  [['*', '*', '*', '*', '*', '*', '*', '*'], 
                         ['*', '*', '*', '*', '*', '*', '*', '*'],
                         ['*', '*', '*', '*', '*', '*', '*', '*'],
                         ['*', '*', '*', 'O', 'X', '*', '*', '*'],
@@ -197,148 +190,3 @@ reversi_start = State [['*', '*', '*', '*', '*', '*', '*', '*'],
                         ['*', '*', '*', '*', '*', '*', '*', '*']] (2, 2, 'X', 'O')
 
 
--- reversi_start = State 
---   [['*', '*', '*', '*', 'X', '*', '*', '*'], 
---    ['*', '*', '*', '*', 'X', 'X', '*', '*'],
---    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'O'],
---    ['X', 'X', 'X', 'X', 'X', 'X', '*', 'O'],
---    ['*', 'X', 'X', 'O', '*', '*', '*', 'O'],
---    ['*', '*', '*', '*', '*', '*', '*', '*'],
---    ['*', '*', '*', '*', '*', '*', '*', '*'],
---    ['*', '*', '*', '*', '*', '*', '*', '*']] (16, 3, 'X', 'O')
-    
-    
--- reversi_start = 
--- --              0    1    2    3    4    5    6    7
--- {-0-}  State [['.', '.', '.', '.', 'O', '.', '.', '.'], 
--- {-1-}         ['.', '.', '.', '.', 'O', 'O', '.', '.'],
--- {-2-}         ['O', 'O', 'O', 'O', 'X', '.', 'O', 'X'],
--- {-3-}         ['.', '.', 'O', 'O', 'O', 'O', '.', 'X'],
--- {-4-}         ['.', '.', 'O', 'O', 'O', '.', '.', 'X'],
--- {-5-}         ['.', '.', '.', '.', '.', '.', '.', '.'],
--- {-6-}         ['.', '.', '.', '.', '.', '.', '.', '.'],
--- {-7-}         ['.', '.', '.', '.', '.', '.', '.', '.']] (15, 4, 'O', 'X')
-
- -- a i = Action i
--- reversi  (Action (5 5)) reversi_start
-
-test_board = 
---      0    1    2    3    4    5    6    7    
-{-0-}[['*', '*', '*', '*', '*', '*', '*', '*'], 
-{-1 -}['*', '*', '*', '*', '*', '*', '*', '*'],
-{-2 -}['*', '*', '*', '*', '*', '*', 'X', '*'],
-{-3 -}['*', '*', '*', 'O', 'X', 'O', 'O', '*'],
-{-4 -}['*', '*', '*', 'X', 'O', 'O', 'X', '*'],
-{-5 -}['*', '*', '*', '*', '*', '*', '*', '*'],
-{-6 -}['*', '*', '*', '*', '*', '*', '*', '*'],
-{-7 -}['*', '*', '*', '*', '*', '*', '*', '*']]
-
-test_State = State [['*', '*', '*', '*', '*', '*', '*', '*'], 
-    ['*', '*', '*', '*', '*', '*', '*', '*'],
-    ['*', '*', '*', '*', '*', '*', 'X', '*'],
-    ['*', '*', '*', 'O', 'X', 'O', 'O', '*'],
-    ['*', '*', '*', 'X', 'O', 'O', '*', '*'],
-    ['*', '*', '*', '*', '*', '*', '*', '*'],
-    ['*', '*', '*', '*', '*', '*', '*', '*'],
-    ['*', '*', '*', '*', '*', '*', '*', '*']] (3, 5, 'X', 'O')
-
--- a i = Action i
--- updateBoard (a (4,6)) test_State  
--- returns State ["********","********","******X*","***OXOX*","***XXXX*","********","********","********"] (2,7,'O','X')
-
-test_end = 
---        0    1    2    3    4    5    6    7
-{-0-}  [['.', '.', '.', '.', 'O', '.', '.', '.'], 
-{-1-}   ['.', '.', '.', '.', 'O', 'O', '.', '.'],
-{-2-}   ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
-{-3-}   ['.', '.', 'O', 'O', 'O', 'O', '.', 'X'],
-{-4-}   ['.', '.', 'O', 'O', 'O', '.', '.', 'X'],
-{-5-}   ['.', '.', '.', '.', '.', '.', '.', '.'],
-{-6-}   ['.', '.', '.', '.', '.', '.', '.', '.'],
-{-7-}   ['.', '.', '.', '.', '.', '.', '.', '.']]
-
--- reversi_start = 
--- --        0    1    2    3    4    5    6    7
--- {-0-}  State [['.', '.', '.', '.', 'O', '.', '.', '.'], 
--- {-1-}   ['.', '.', '.', '.', 'O', 'O', '.', '.'],
--- {-2-}   ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'X'],
--- {-3-}   ['.', '.', 'O', 'O', 'O', 'O', '.', 'X'],
--- {-4-}   ['.', '.', 'O', 'O', 'O', '.', '.', 'X'],
--- {-5-}   ['.', '.', '.', '.', '.', '.', '.', '.'],
--- {-6-}   ['.', '.', '.', '.', '.', '.', '.', '.'],
--- {-7-}   ['.', '.', '.', '.', '.', '.', '.', '.']] (3, 17, 'X', 'O')
-
-test_end_star = 
-   [['*', '*', '*', '*', 'X', '*', '*', '*'], 
-    ['*', '*', '*', '*', 'X', 'X', '*', '*'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'O'],
-    ['*', '*', 'X', 'X', 'X', 'X', '*', 'O'],
-    ['*', '*', 'X', 'X', 'X', '*', '*', 'O'],
-    ['*', '*', '*', '*', '*', '*', '*', '*'],
-    ['*', '*', '*', '*', '*', '*', '*', '*'],
-    ['*', '*', '*', '*', '*', '*', '*', '*']]
-
-test_update = 
-    State [['*', '*', '*', '*', 'X', '*', '*', '*'], 
-           ['*', '*', '*', '*', 'O', 'X', '*', '*'],
-           ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'O'],
-           ['*', '*', 'X', 'X', 'X', 'X', '*', 'O'],
-           ['*', '*', 'X', 'X', 'X', '*', '*', 'O'],
-           ['*', '*', '*', '*', '*', '*', '*', '*'],
-           ['*', '*', '*', '*', '*', '*', '*', '*'],
-           ['*', '*', '*', '*', '*', '*', '*', '*']] (17, 3, 'O','X')
-
-brd = 
-   [['*', '*', '*', '*', 'X', '*', '*', '*'], 
-    ['*', '*', '*', '*', 'X', 'X', '*', '*'],
-    ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'O'],
-    ['X', 'X', 'X', 'X', 'X', 'X', '*', 'O'],
-    ['X', 'X', 'X', 'X', 'X', '*', '*', 'O'],
-    ['*', '*', '*', '*', '*', '*', '*', '*'],
-    ['*', '*', '*', '*', '*', '*', '*', '*'],
-    ['*', '*', '*', '*', '*', '*', '*', '*']]
-
--- reversi_start = 
---    State [['*', '*', '*', '*', 'X', '*', '*', '*'], 
---     ['*', '*', '*', '*', 'X', 'X', '*', '*'],
---     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'O'],
---     ['*', '*', 'X', 'X', 'X', 'X', '*', 'O'],
---     ['*', '*', 'X', 'O', '*', '*', '*', 'O'],
---     ['*', '*', '*', '*', '*', '*', '*', '*'],
---     ['*', '*', '*', '*', '*', '*', '*', '*'],
---     ['*', '*', '*', '*', '*', '*', '*', '*']] (17, 3, 'X','O')
-
--- reversi_start = 
---    State [['*', '*', '*', '*', 'X', '*', '*', 'X'], 
---     ['*', '*', '*', '*', 'X', 'X', '*', 'X'],
---     ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'O'],
---     ['*', '*', 'X', 'X', 'X', 'X', '*', 'O'],
---     ['*', '*', 'X', 'O', '*', '*', '*', 'O'],
---     ['*', '*', '*', '*', '*', '*', '*', '*'],
---     ['*', '*', '*', '*', '*', '*', '*', '*'],
---     ['*', '*', '*', '*', '*', '*', '*', '*']] (17, 3, 'X','O')    
-
-test_skip_board = 
---        0    1    2    3    4    5    6    7
-{-0-}  [['*', '*', '*', '*', '*', '*', '*', '*'], 
-{-1-}   ['*', '*', '*', '*', 'O', 'O', '*', '*'],
-{-2-}   ['O', 'O', 'O', 'O', 'O', 'O', 'O', '*'],
-{-3-}   ['*', '*', 'O', 'O', 'O', 'O', '*', 'O'],
-{-4-}   ['*', '*', 'O', 'O', 'O', '*', '*', 'X'],
-{-5-}   ['*', '*', '*', '*', '*', '*', '*', '*'],
-{-6-}   ['*', '*', '*', '*', '*', '*', '*', '*'],
-{-7-}   ['*', '*', '*', '*', '*', '*', '*', '*']]-- to test this change 
-
--- reversi_start = 
--- -- test_State_skip = 
--- --        0    1    2    3    4    5    6    7
--- {-0-}  State [['*', '*', '*', '*', '*', '*', '*', 'O'], 
--- {-1-}   ['*', '*', '*', '*', 'O', 'O', '*', 'O'],
--- {-2-}   ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
--- {-3-}   ['*', '*', 'O', 'O', 'O', 'O', '*', 'O'],
--- {-4-}   ['*', '*', 'O', 'O', 'X', '*', '*', 'X'],
--- {-5-}   ['*', '*', '*', '*', '*', '*', '*', '*'],
--- {-6-}   ['*', '*', '*', '*', '*', '*', '*', '*'],
--- {-7-}   ['*', '*', '*', '*', '*', '*', '*', '*']] (1, 17, 'O', 'X')
-
---reversi_start = (State test_skip_board (1,17,'X','O'))
--- playReversiMerci.hs
+++ playReversiMerci.hs
@@ -3,11 +3,14 @@ module Play where
 import ReversiMerci
 
 import System.IO
+import Data.Time.Clock
 import Text.Read   (readMaybe)
 
 --to play
 -- play reversi reversi_start 'X' (0,0,0)
 
+--get user input and check that they've given an Int
+get_ans :: Read b => String -> IO b
 get_ans s = 
     do
         putStr s
@@ -21,22 +24,26 @@ get_ans s =
 get_num_ans :: (Read b, Num b) => String -> IO b
 get_num_ans = get_ans
 
---switch_opp p1
---    |p1=='X' = 'O'
---    |otherwise = 'X'
 
+--positions to show on the board for to make choosing moves easier
 col_position_arr = ['0','1','2','3','4','5','6','7']
 position_arr = ['+','0','1','2','3','4','5','6','7']
+
+--append the row number to each row in the board
+number_rows :: [[Char]] -> [[Char]]
 number_rows board = [y:x | (x,y) <- (zip board position_arr)]
+
+--show board appending the row and column numbers
+--using logic from 2019 connect 4 project logic and  https://stackoverflow.com/questions/18691321/printing-a-2d-array-in-haskell
 show_numbered_board :: [[Char]] -> IO ()
 show_numbered_board board= 
     let num_board = number_rows (col_position_arr:board)
     in putStr (unlines [unwords [show (num_board !! y !! x) | x <- [0..8]] | y <- [0..8]])
 
+type CurrentGameStatus = (Int,Int,Int)   -- wins for X, wins for O, ties
 
-type CurrentGameStatus = (Int,Int,Int)   -- wins, losses, ties
+--main function to start playing games
 play :: Game -> State -> Player -> CurrentGameStatus -> IO CurrentGameStatus
-
 play game start_state opponent ts =
   let (x_wins, o_wins,ties) = ts in
   do
@@ -47,7 +54,16 @@ play game start_state opponent ts =
         then
             person_play game (ContinueGame reversi_start) opponent ts "pvp"
         else if line ==  "1"
-             then person_play game (ContinueGame reversi_start) opponent ts "pvcomp" 
+             then 
+              do 
+                putStrLn "Choose level: 0=Beginner, 1= Intermediate, 2 = Advanced"
+                level <- getLine
+                if level == "1"
+                  then person_play game (ContinueGame reversi_start) opponent ts "pvcomp" 
+                  else if level == "2"
+                    then person_play game (ContinueGame reversi_start) opponent ts "pvcompH" 
+                  else 
+                    person_play game (ContinueGame reversi_start) opponent ts "pvcompB"
         else if line == "2"
             then return ts
         else play game reversi_start opponent ts
@@ -55,9 +71,13 @@ play game start_state opponent ts =
 
 person_play :: Game -> Result -> Player -> CurrentGameStatus -> [Char] -> IO CurrentGameStatus
 -- opponent has played, the person must now play
+-- Includes a punishment-based timer which causes the player's turn to be skipped
+-- if they have exceeded a certain amount of time when they make their move
 
 person_play game (ContinueGame state) opponent ts opp_type =
    do
+      let time = 30
+      currTime <- getCurrentTime  --variable to instantiate original time
       let State board (int1,int2, char1, char2)  = state
       show_numbered_board board
       putStrLn ("current player: "++show char1)
@@ -65,14 +85,39 @@ person_play game (ContinueGame state) opponent ts opp_type =
       do 
         rowline <- get_num_ans "choose a row: "
         colline <- get_num_ans "choose a column: "
-        if opp_type=="pvcomp"
-            then computer_play game (game (Action (rowline, colline)) state) opponent ts opp_type  
-        else person_play game (game (Action (rowline, colline)) state) opponent ts opp_type
+        currTimeNew <- getCurrentTime  
+        if opp_type=="pvcomp" || opp_type == "pvcompH"
+            then 
+                if diffUTCTime currTimeNew currTime >=  time
+                    then 
+                    do 
+                        putStrLn "You took too long to make your move..it is now your opponent's turn"
+                        computer_play game (ContinueGame (State board (int2, int1, char2, char1))) opponent ts opp_type
+                    else 
+                    computer_play game (game (Action (rowline, colline)) state) opponent ts opp_type
+            else if opp_type == "pvcompB"
+                then  
+                    if diffUTCTime currTimeNew currTime >=  time
+                        then 
+                        do 
+                            putStrLn "You took too long to make your move..it is now your opponent's turn"
+                            computer_playB game (ContinueGame (State board (int2, int1, char2, char1))) opponent ts opp_type 
+                        else
+                            computer_playB game (game (Action (rowline, colline)) state) opponent ts opp_type
+            else 
+                if diffUTCTime currTimeNew currTime >=  time 
+                    then 
+                    do 
+                        putStrLn "You took too long to make your move..it is now your opponent's turn"
+                        person_play game (ContinueGame (State board (int2, int1, char2, char1))) opponent ts opp_type
+                    else 
+                        person_play game (game (Action (rowline, colline)) state) opponent ts opp_type
         
 person_play game (EndOfGame val start_state) opponent ts opp_type =
   do
     newts <- update_status val ts  
     play game start_state opponent newts
+
 
 computer_play :: Game -> Result -> Player -> CurrentGameStatus -> [Char] -> IO CurrentGameStatus
 computer_play game (EndOfGame val  start_state) opponent ts opp_type =
@@ -80,18 +125,43 @@ computer_play game (EndOfGame val  start_state) opponent ts opp_type =
       newts <- update_status val ts
       play game start_state opponent newts
 
+-- this computer_play plays the intermediate or advanced version of the computer's play
+-- based on the opp_type
 computer_play game (ContinueGame state) opponent ts opp_type =
       let 
           State board (int1,int2, char1, char2)  = state
-          next_move = (Action (choose_best state))
+          next_move = (Action (choose_best state opp_type))
         in
             if (opp_type=="pvcomp") && (char1 /= 'O') -- make sure person played valid move and players have switched since it's person vs comp, computer is always second so it will be O
-            then person_play game (ContinueGame state) opponent ts opp_type
-            else 
+                then person_play game (ContinueGame state) opponent ts opp_type
+            else if (opp_type=="pvcompH") && (char1 /= 'O')
+                then person_play game (ContinueGame state) opponent ts opp_type
+            else
             do
                 putStrLn ("The computer chose "++show next_move)
                 person_play game (game next_move state) opponent ts opp_type
 
+
+
+--computer_playB is the easiest computer player, it chooses a valid move randomly
+computer_playB :: Game -> Result -> Player -> CurrentGameStatus -> [Char] -> IO CurrentGameStatus
+computer_playB game (EndOfGame val  start_state) opponent ts opp_type =
+   do
+      newts <- update_status val ts
+      play game start_state opponent newts
+
+--this is when the computer plays the beginner version of difficulty
+computer_playB game (ContinueGame state) opponent ts opp_type =
+      let 
+          State board (int1,int2, char1, char2)  = state
+          next_moveB = (Action (returnValidMove (createCoordinate [0..7] [0..7]) board char1 char2))
+        in
+            if (opp_type=="pvcompB") && (char1 /= 'O') -- make sure person played valid move and players have switched since it's person vs comp, computer is always second so it will be O
+            then person_play game (ContinueGame state) opponent ts opp_type
+            else 
+            do
+                putStrLn ("The computer chose "++show next_moveB)
+                person_play game (game next_moveB state) opponent ts opp_type
 
 
 --list all valid moves for current player
@@ -99,6 +169,8 @@ computer_play game (ContinueGame state) opponent ts opp_type =
 list_valid_moves :: State -> [(Int, Int)]
 list_valid_moves (State board (int1,int2, char1, char2)) = [x | x <- (createCoordinate [0..7] [0..7]) , valid x board char1 char2]
 
+
+-- chooses next best coordinates for intermediate person vs. computer game
 choose_best_helper:: [(Int, Int)] -> State -> (Int, Int) -> Int -> (Int, Int)
 choose_best_helper [] _ so_far _ = so_far
 choose_best_helper (h:t) (State board (int1,int2, char1, char2)) best_coord best_val = 
@@ -107,19 +179,51 @@ choose_best_helper (h:t) (State board (int1,int2, char1, char2)) best_coord best
     else choose_best_helper t (State board (int1,int2, char1, char2)) best_coord best_val
     where (State new_board (new_int1,new_int2, new_char1, new_char2)) = (updateBoard h (State board (int1,int2, char1, char2)))
 
---return best move for that state (for current player)
---choose_best test_State => (4,6)
-choose_best :: State -> (Int, Int)
-choose_best state = choose_best_helper (list_valid_moves state) state (-1,-1) 0
+--takes list of valid moves for current state and the current state 
+--return list of tuples (move, state after opponent makes best move given your move)
+look_ahead :: [(Int, Int)] -> State -> [((Int, Int), State)]
+look_ahead [] _ = []
+look_ahead (h:t) state = 
+    let new_state = updateBoard h state
+    in (h, (updateBoard (choose_best_helper (list_valid_moves new_state) new_state (8,8) 0) new_state)):(look_ahead t state)
 
+-- given the list of moves and associated states from look_ahead, return the best move that will result in the current player having the highest score possible for them on their next move
+best_look_ahead :: [(t, State)] -> t -> Int -> t
+best_look_ahead [] so_far _= so_far
+best_look_ahead ((coord, state):t) best_coord best_val =
+--state is currently back to your next move after opponent made their best move given your current coord
+    let (State new_board (new_int1,new_int2, new_char1, new_char2)) = updateBoard (choose_best_helper (list_valid_moves state) state (8,8) 0) state
+    in if new_int2 > best_val
+        then best_look_ahead t coord new_int2 
+        else best_look_ahead t best_coord best_val
+
+
+--return best move for that state (for current player) or for next state in the
+-- case of advanced version: depends on opp_type
+--choose_best test_State => (4,6)
+choose_best :: State -> [Char]  -> (Int, Int)
+choose_best state opp_type = 
+    if opp_type == "pvcompH"
+        then best_look_ahead (look_ahead (list_valid_moves state) state) (8,8) 0 
+    else  
+        choose_best_helper (list_valid_moves state) state (8,8) 0
+
+--update the current game stats and show who won the current game
 update_status:: Char -> CurrentGameStatus -> IO CurrentGameStatus
 update_status val (x_wins,o_wins,ties)
   | val == 'X' = do
       putStrLn "X Won"
+      putStrLn "Merci for the Reversi"
       return (x_wins+1, o_wins,ties)
   | val == 't' = do
       putStrLn "It's a tie"
+      putStrLn "Merci for the Reversi"
       return (x_wins, o_wins,ties+1)
   | otherwise = do
       putStrLn "O won!"
+      putStrLn "Merci for the Reversi"
       return (x_wins, o_wins+1,ties)
+
+
+
+            
